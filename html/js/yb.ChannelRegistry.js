b79ac078da4d840bc2c8a22d5ce6ce5fbbb61578
Zach Hoeken <hoeken@gmail.com>
2025-10-27 19:53:32 -1000

    reorganize the js files

--- html/js/yb.ChannelRegistry.js
+++ html/js/yb.ChannelRegistry.js
@@ -1,30 +1,43 @@
 var YB = typeof YB !== "undefined" ? YB : {};
 
-// Simple type registry + factory
-var YBChannelRegistry = {
-  "adc": YB.ADCChannel,
-  "dio": YB.DigitalInputChannel,
-  "pwm": YB.PWMChannel,
-  "relay": YB.RelayChannel,
-  "servo": YB.ServoChannel,
-  "stepper": YB.StepperChannel,
-};
+// Channel registry + factory under YB
+YB.ChannelRegistry = {
+  // Map of type keys to constructor functions
+  channels: {
+    "adc": YB.ADCChannel,
+    "dio": YB.DigitalInputChannel,
+    "pwm": YB.PWMChannel,
+    "relay": YB.RelayChannel,
+    "servo": YB.ServoChannel,
+    "stepper": YB.StepperChannel
+  },
 
-YB.registerChannelType = function (type, ctor) {
-  YBChannelRegistry[type] = ctor;
-};
+  // Register or override a channel type
+  registerChannelType: function (type, ctor) {
+    this.channels[type] = ctor;
+  },
 
-YB.channelFromConfig = function (cfg, type) {
-  if (!cfg) {
-    throw new Error('Channel config must include a "type".');
+  // Create a channel instance from a config object
+  channelFromConfig: function (cfg, type) {
+    if (!cfg) {
+      throw new Error('Invalid channel config.');
+    }
+
+    var ctor = this.channels[type];
+    if (!ctor) {
+      throw new Error("Unknown channel type: " + type);
+    }
+
+    var instance = new ctor();
+
+    // Prefer instance method if available
+    if (typeof instance.parseConfig === "function") {
+      instance.parseConfig(cfg);
+    } else if (typeof ctor.parseConfig === "function") {
+      // Fallback if you keep a static parseConfig on the constructor
+      ctor.parseConfig(cfg, instance);
+    }
+
+    return instance;
   }
-  var channel = YBChannelRegistry[type];
-  if (!channel) {
-    throw new Error("Unknown channel type: " + type);
-  }
-
-  let myChannel = new channel();
-  channel.parseConfig(cfg);
-
-  return channel;
 };
