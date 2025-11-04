5e2d55f53d0985efbcc33fca97dc50a89c1fa5b7
Zach Hoeken <hoeken@gmail.com>
2025-11-04 09:46:10 -0600

    moved all the library js includes into their own folder

--- html/libs/stub.js
+++ html/libs/stub.js
@@ -1,3 +1,3 @@
-//generate thsi with: browserify html/stub.js > html/yarrboard-client.js
+//generate thsi with: browserify html/libs/stub.js > html/libs/yarrboard-client.js
 const YarrboardClient = require('yarrboard-client');
 window.YarrboardClient = YarrboardClient;
