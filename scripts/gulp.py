b8fdcb56598665a232e79917a4680ebaa7327dbf
Zach Hoeken <hoeken@gmail.com>
2026-01-04 12:34:06 +0700

    updated to v2.0.0 release of yarrboard framework.

--- scripts/gulp.py
+++ scripts/gulp.py
@@ -1,16 +1,7 @@
 import os
+import glob
 Import("env")
 
-#
-# Build actions
-#
-
-# def before_build(source, target, env):
-#     print("Compressing web app into header")
-#     env.Execute("gulp")
-
-#env.AddPreAction("buildprog", before_build)
-#env.AddPreAction("$BUILD_DIR/src/server.cpp.o", before_build)
 
 #print("Packaging yarrboard-client from NPM")
 #env.Execute("browserify html/stub.js > html/yarrboard-client.js")
